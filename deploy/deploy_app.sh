0805806b5a6e03aa9fa0ce636f79e034d07a4916
Lehel Kovach <drdorianwhite@gmail.com>
2017-12-26 18:18:08 -0800

    fixed script error

--- deploy/deploy_app.sh
+++ deploy/deploy_app.sh
@@ -15,10 +15,7 @@ if ! [ -x "$(command -v docker)" ]; then
   echo "IMPROTANT:  adding current user to docker group.  docker daemon will not be able to be started until host machine has loggeed out and back in!"
   sudo usermod -aG docker $USER
   echo "user modified.  attempting to logout.  Log back in with same user; the rest of this shell script will continue execution upon logging in again!"
-  cp ./deploy_app.sh ./deploy_app_contd.sh
-  sudo chmod a+x ./deploy_app_contd.sh
-  echo "sudo rum /etc/profile.d/deploy_app_contd.sh" >> deploy_app_contd.sh
-  sudo mv deploy_app_contd.sh /etc/profile.d/
+  sudo cp run-containers-on-login.sh /etc/profile.d/
   echo "Logging this user out.  Please log back in to contiue deployment...."
   sudo pkill -u $USER
   exit
@@ -32,7 +29,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   echo "docker-compose not installed.  using curl to run online shell scripts..."
   sudo curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
-if
+fi
 
 
 sh ./run-containers.sh
